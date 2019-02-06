cefdbdf38d11c45b88d43e8ac9b1d72ebd80a6e2
colledkm <colledkm@rose-hulman.edu>
2019-02-06 09:48:33 -0500

    Added test code for driving to m2

--- src/m2_run_this_on_robot.py
+++ src/m2_run_this_on_robot.py
@@ -16,6 +16,39 @@ def main():
       1. Makes the EV3 robot to various things.
       2. Communicates via MQTT with the GUI code that runs on the LAPTOP.
     """
+    test_go()
+    test_stop()
+    test_go_straight_for_seconds()
+    test_go_straight_for_inches_using_time()
+
+def test_go():
+    robot = rosebot.RoseBot()
+    robot.go()
+
+def test_stop():
+    robot = rosebot.RoseBot()
+    robot.stop()
+
+def test_go_straight_for_seconds():
+    robot = rosebot.RoseBot()
+    robot.go_straight_for_seconds(5, 100)
+    robot.go_straight_for_seconds(5,-100)
+    robot.go_straight_for_seconds(0,10)
+    robot.go_straight_for_seconds(3,20)
+    robot.go_straight_for_seconds(3,0)
+    robot.go_straight_for_seconds(3,-20)
+
+def test_go_straight_for_inches_using_time():
+    robot = rosebot.RoseBot()
+    robot.go_straight_for_inches_using_time(10,100)
+    robot.go_straight_for_inches_using_time(10,-100)
+    robot.go_straight_for_inches_using_time(5,25)
+    robot.go_straight_for_inches_using_time(0,75)
+    robot.go_straight_for_inches_using_time(3,60)
+
+
+
+
 
 
 # -----------------------------------------------------------------------------
