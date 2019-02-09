859da02fe9d0282c515e121f8bfac9953f9b4edb
colledkm <colledkm@rose-hulman.edu>
2019-02-08 20:36:21 -0500

    why you no work go striaght for inches using encoder? aka I added print statements

--- sandbox/sandbox2.py
+++ sandbox/sandbox2.py
@@ -1,3 +1,18 @@
 # Put whatever you want in this module and do whatever you want with it.
 # It exists here as a place where you can "try out" things without harm.
 
+def go_straight_for_inches_using_encoder(inches, speed):
+    """
+    Makes the robot go straight (forward if speed > 0, else backward)
+    at the given speed for the given number of inches,
+    using the encoder (degrees traveled sensor) built into the motors.
+    """
+    inches_per_degree = 30 / 360
+    degree = inches / inches_per_degree
+    left_motor
+    self.go(speed, speed)
+    while True:
+        position = self.left_motor.get_position()
+        if abs(position) >= degree:
+            break
+    self.stop()
