5f386e6b7cefbace93ff14c96cd4f52c6e48ad04
kellyzc <kellyzc@rose-hulman.edu>
2019-02-11 08:31:56 -0500

    update to m3 files

--- src/m3_run_this_on_laptop.py
+++ src/m3_run_this_on_laptop.py
@@ -46,7 +46,7 @@ def main():
     # Sub-frames for the shared GUI that the team developed:
     # -------------------------------------------------------------------------
 
-    teleop_frame, arm_frame, control_frame = get_shared_frames(main_frame, sender)
+    teleop_frame, arm_frame, control_frame, sprint_1_drive_system, sprint_1_beeper = get_shared_frames(main_frame, sender)
 
     # -------------------------------------------------------------------------
     # Frames that are particular to my individual contributions to the project.
@@ -57,7 +57,7 @@ def main():
     # Grid the frames.
     # -------------------------------------------------------------------------
 
-    grid_frames(teleop_frame, arm_frame, control_frame)
+    grid_frames(teleop_frame, arm_frame, control_frame, sprint_1_drive_system, sprint_1_beeper)
 
     # -------------------------------------------------------------------------
     # The event loop:
@@ -70,14 +70,18 @@ def get_shared_frames(main_frame, mqtt_sender):
     teleop_frame = shared_gui.get_teleoperation_frame(main_frame, mqtt_sender)
     arm_frame = shared_gui.get_arm_frame(main_frame, mqtt_sender)
     control_frame = shared_gui.get_control_frame(main_frame, mqtt_sender)
+    sprint_1_drive_system = shared_gui.get_Sprint_1_Drive_System_frame(main_frame, mqtt_sender)
+    sprint_1_beeper = shared_gui.get_Sprint_1_Beeper_System_frame(main_frame, mqtt_sender)
 
-    return teleop_frame, arm_frame, control_frame
+    return teleop_frame, arm_frame, control_frame, sprint_1_drive_system, sprint_1_beeper
 
 
-def grid_frames(teleop_frame, arm_frame, control_frame):
+def grid_frames(teleop_frame, arm_frame, control_frame, sprint_1_drive_system, sprint_1_beeper):
     teleop_frame.grid(row=0, column=0)
     arm_frame.grid(row=1, column=0)
-    control_frame.grid(row=2, column=0)
+    sprint_1_drive_system.grid(row=2, column=0)
+    sprint_1_beeper.grid(row=3, column=0)
+    control_frame.grid(row=4, column=0)
 
 
 # -----------------------------------------------------------------------------
