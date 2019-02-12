8f2881db9ca2f06e176a5ea5d6095790661e4250
colledkm <colledkm@rose-hulman.edu>
2019-02-12 17:11:11 -0500

    Working on gui in my sandbox

--- sandbox/sandbox2.py
+++ sandbox/sandbox2.py
@@ -34,20 +34,46 @@ def get_IR_frame(window, mqtt_sender):
     frame_label.grid(row=0, column=1)
     distance_is_less_than_label.grid(row=1, column=0)
     distance_is_less_than_label.grid(row=1, column=1)
-    tone_frequency_label.grid(row=1, column=2)
-    speak_text_label.grid(row=1, column=3)
+    distance_is_greater_than_label.grid(row=1, column=2)
+    distance_is_within_label.grid(row=1, column=3)
 
     distance_less.grid(row=2, column=0)
-    tone_duration.grid(row=2, column=1)
-    tone_frequency.grid(row=2, column=2)
-    speak_text.grid(row=2, column=3)
+    distance_greater.grid(row=2, column=1)
+    distance_within.grid(row=2, column=2)
 
     distance_less_button.grid(row=3, column=0)
-    tone_button.grid(row=3, column=1)
-    speak_button.grid(row=3, column=3)
+    distance_greater_button.grid(row=3, column=1)
+    distance_within_button.grid(row=3, column=2)
 
     # Set the button callbacks:
-    distance_less_button["command"] = lambda: handle_beep_button(number_of_beeps.get(), mqtt_sender)
-    tone_button["command"] = lambda: handle_tone_button(tone_duration.get(), tone_frequency.get(), mqtt_sender)
-    speak_button["command"] = lambda: handle_speak_button(speak_text.get(), mqtt_sender)
-    return frame
+    distance_less_button["command"] = lambda: handle_distance_less_button(distance_less.get(),wheel_speed_entry.get(), mqtt_sender)
+    distance_greater_button["command"] = lambda: handle_distance_greater_button(distance_greater.get(),wheel_speed_entry.get(), mqtt_sender)
+    distance_within_button["command"] = lambda: handle_distance_within_button(distance_within.get(),wheel_speed_entry.get(), mqtt_sender)
+    return
+
+##############################################################
+def handle_distance_less_button(inches,speed,mqtt_sender):
+    mqtt_sender.send_message('distance_less', [str(inches), str(speed)])
+    print('distance_less', inches, speed)
+
+def handle_distance_greater_button(inches,speed,mqtt_sender):
+    mqtt_sender.send_message('distance_greater', [str(inches), str(speed)])
+    print('distance_greater', inches, speed)
+
+def handle_distance_within_button(inches,speed,mqtt_sender):
+    mqtt_sender.send_message('distance_within', [str(inches), str(speed)])
+    print('distance_within', inches, speed)
+
+##################################################################
+def distance_less(self,inches,speed):
+    print("Command Recieved: distance_less")
+    self.robot.drive_system.go_forward_until_distance_is_less_than(int(inches),int(speed))
+
+def distance_greater(self,inches,speed):
+    print("Command Recieved: distance_greater")
+    self.robot.drive_system.go_backward_until_distance_is_greater_than(int(inches),int(speed))
+
+def distance_greater(self, inches, speed):
+    print("Command Recieved: distance_greater")
+    self.robot.drive_system.go_backward_until_distance_is_greater_than(int(inches), int(speed))
+go_until_distance_is_within
