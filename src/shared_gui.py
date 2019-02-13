b5abecfc436dc4faf87307d3d1b03dde97d55b0e
colledkm <colledkm@rose-hulman.edu>
2019-02-13 09:04:43 -0500

    Working on feature 8 and 9 gui

--- src/shared_gui.py
+++ src/shared_gui.py
@@ -413,6 +413,67 @@ def handle_speak_button(text,mqtt_sender):
     print('speak_button', text)
 
 
+def get_Katana_frame(window, mqtt_sender):
+    """
+    Constructs and returns a frame on the given window, where the frame
+    has Beeper objects that control the EV3 robot's motion
+    by passing messages using the given MQTT Sender.
+      :type  window:       ttk.Frame | ttk.Toplevel
+      :type  mqtt_sender:  com.MqttClient
+    """
+    # Construct the frame to return:
+    frame = ttk.Frame(window, padding=10, borderwidth=5, relief="ridge")
+    frame.grid()
+
+    # Construct the widgets on the frame:
+    frame_label = ttk.Label(frame, text="Katana's System")
+    obtain_with_sensor_label = ttk.Label(frame, text="Pick Up Object")
+    obtain_with_sensor_button = ttk.Button(frame, text="Get")
+
+    rate_of_frequency_label = ttk.Label(frame, text="Frequency Rate (Increasing)")
+    rate_of_frequency = ttk.Entry(frame, width=8, justify=tkinter.LEFT)
+    rate_of_frequency.insert(0, "10")
+    initial_frequency_label = ttk.Label(frame, text="Initial Frequency")
+    initial_frequency = ttk.Entry(frame, width=8, justify=tkinter.LEFT)
+    initial_frequency.insert(0, "5")
+
+    obtain_with_camera_label = ttk.Label(frame, text="Pick up Object with Camera")
+    obtain_with_camera_button = ttk.Button(frame, text="Get")
+
+    spin_speed_label = ttk.Label(frame, text="Spinning Speed")
+    spin_speed = ttk.Entry(frame, width=8, justify=tkinter.LEFT)
+    spin_speed.insert(0, "10")
+    spin_direction_label = ttk.Label(frame, text="Spinning Direction")
+    spin_direction = ttk.Entry(frame, width=8, justify=tkinter.LEFT)
+    spin_direction.insert(0, "Counter Clockwise")
+
+    # Grid the widgets:
+    frame_label.grid(row=0, column=1)
+    obtain_with_sensor_label.grid(row=1, column=0)
+    obtain_with_sensor_button.grid(row=1, column=1)
+    rate_of_frequency.grid(row=2, column=0)
+    rate_of_frequency.grid(row=2, column=1)
+    initial_frequency.grid(row=3, column=0)
+    initial_frequency.grid(row=3, column=1)
+
+    obtain_with_camera_label.grid(row=4, column=0)
+    obtain_with_camera_button.grid(row=4, column=1)
+    spin_speed_label.grid(row=5, column=0)
+    spin_speed.grid(row=5, column=1)
+    spin_direction.grid(row=6, column=0)
+    spin_direction.grid(row=6, column=1)
+
+    # Set the button callbacks:
+    #obtain_with_sensor_button["command"] = lambda: handle_obtain_with_sensor_button(rate_of_frequency.get(),
+                                                                                    initial_frequency.get(),
+                                                                                    mqtt_sender)
+    #obtain_with_camera_button["command"] = lambda: handle_obtain_with_camera_button(spin_speed.get(),
+                                                                                    spin_direction.get(),
+                                                                                    rate_of_frequency.get(),
+                                                                                    initial_frequency.get(),
+                                                                                    mqtt_sender)
+    return frame
+
 
 def get_Nick_frame(window, mqtt_sender):
     """
