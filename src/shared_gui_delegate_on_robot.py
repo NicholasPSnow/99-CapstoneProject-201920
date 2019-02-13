cc33171e8925066f95aef8a60ad9e58a4a17c47d
NicholasPSnow <44656056+NicholasPSnow@users.noreply.github.com>
2019-02-13 08:19:11 -0500

    Personal Sprint Stuff

--- src/shared_gui_delegate_on_robot.py
+++ src/shared_gui_delegate_on_robot.py
@@ -120,11 +120,13 @@ class DelegateThatReceives(object):
         self.robot.arm_and_claw.lower_arm()
         int_initial_beeps=int(initial_beeps)
         int_rate_of_beeps=int(rate_of_beeps)
+
         while self.robot.sensor_system.ir_proximity_sensor.get_distance_in_inches()>2:
             self.robot.drive_system.go(75,75)
-            time.sleep(int_initial_beeps/(int_rate_of_beeps*(1/self.robot.sensor_system.ir_proximity_sensor.get_distance_in_inches())))
+            time.sleep((int(initial_beeps)/(int(rate_of_beeps)*self.robot.sensor_system.ir_proximity_sensor.get_distance_in_inches())))
             self.robot.sound_system.beeper.beep()
         self.robot.arm_and_claw.raise_arm()
+        self.robot.drive_system.stop()
     def m1camera_button(self,speed,direction,rate_of_beeps, initial_beeps):
         print("Command Recieved: Camera")
 
